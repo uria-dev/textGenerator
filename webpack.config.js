7ef2b285d7e0ae2c2cfc8005b9f662e512bec0d8
Uria <uria@urialevy.com>
2024-12-09 13:44:11 +0000

    Initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const path = require("path");
-
+const path = require('path')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+    mode: 'development',
+    entry: { entry: './src/app.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-};
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+}
